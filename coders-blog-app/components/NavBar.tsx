c6a7c8a5bb132a4646142ec7b5d65837877b13ea
sumanacharyyagit <sumanacharyya999@gmail.com>
2023-03-01 04:28:34 +0530

    Added Footer and Header

--- coders-blog-app/components/NavBar.tsx
+++ coders-blog-app/components/NavBar.tsx
@@ -1,7 +1,49 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
-  return <nav>This is our Nav</nav>;
+  return (
+    <nav className="flex items-center justify-between py-6 px-2">
+      <Link href={"/"}>
+        <div className="flex items-center cursor-pointer">
+          <Image src="/logo.png" alt="logo" width={50} height={50} />
+          <span className="font-bold ml-3 text-primary">
+            {"Coder's"} Blog App
+          </span>
+        </div>
+      </Link>
+      <ul className="flex items-center">
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#">Products</a>
+        </li>
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#">Pricing</a>
+        </li>
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#">Docs</a>
+        </li>
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#">Company</a>
+        </li>
+      </ul>
+      <ul className="flex items-center">
+        <li className="mr-6 font-medium text-gray-600">
+          <a href="#" className="hover:text-gray-400 transition-all">
+            Login
+          </a>
+        </li>
+        <li className="font-medium text-gray-600">
+          <a
+            href="#"
+            className="bg-primary px-4 py-2 rounded-md text-white hover:bg-primary-dark transition-all"
+          >
+            Signup
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
